47e3a2c864e08e9de7b198a905b1338aa23cc48b
Brandon B. Nguyen <18nguyenb20@millshighsch.org>
2017-05-11 23:12:08 -0700

    set up a player class, nothing really yet though

---
+++ TapOfWar/src/Player.java
@@ -0,0 +1,20 @@
+
+public class Player {
+	private String character;
+	
+	public Player(String character){
+		this.character = character;
+	}
+	
+	public String getCharacter(){
+		return character;
+	}
+	
+	public int getStamina(){
+		return 0;
+	}
+	
+	public int getPullStrength(){
+		return 0;
+	}
+}
