28ec6028d5c1065bb52d32813e883a40aa9603ba
Brandon B. Nguyen <18nguyenb20@millshighsch.org>
2017-05-14 21:17:34 -0700

    nothing much

---
+++ TestingSwing/src/test.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class test {
+	public static void main(String[] args){
+		JFrame frame = new JFrame("Tap of War");
+		frame.setSize(500,400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
